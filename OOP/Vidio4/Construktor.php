e521e5728a39abf2f692b7a5eded626497ddcb12
Annisaseptiyani <annisa.223040172@mail.unpas.ac.id>
2024-10-13 23:41:48 +0700

    Tugas vidio

---
+++ OOP/Vidio4/Construktor.php
@@ -0,0 +1,30 @@
+<?php 
+
+class Produk{
+  public $judul = "judul",//Property
+        $penulis = "penulis",
+        $penerbit = "penerbit",
+        $harga = "harga";
+
+        public function __construct($judul,$penulis,$penerbit,$harga){
+          $this ->judul = $judul;
+          $this ->penulis = $penulis;
+          $this ->penerbit = $penerbit;
+          $this ->harga = $harga;
+        }
+
+        public function getLabel(){//Function/method
+            return 
+            "$this->penulis,
+            $this->penerbit";
+        }
+}
+
+$produk1 = new Produk("Narutu","Masahi","Shonen jump","30000");//objek 
+$produk2 = new produk("Atomic Habbit","james clear","indonesia",40000);
+
+
+echo "komik : $produk1-> penulis, $produk1 -> penerbit";
+echo "<br>";
+echo $produk2->getLabel()
+ ?>
---
+++ OOP/Vidio5/ObjectType.php
@@ -0,0 +1,40 @@
+<?php 
+
+class Produk{
+  public $judul,//Property
+        $penulis,
+        $penerbit,
+        $harga;
+
+        public function __construct($judul,$penulis,$penerbit,$harga){
+          $this ->judul = $judul;
+          $this ->penulis = $penulis;
+          $this ->penerbit = $penerbit;
+          $this ->harga = $harga;
+        }
+
+        public function getLabel(){//Function/method
+            return 
+            "$this->penulis,
+            $this->penerbit";
+        }
+}
+
+class CetakInfoTerbaru{
+    public function cetak(Produk $produk){
+    $str = "{$produk-> judul} | {$produk ->getLabel()}(Rp. {$produk-> harga})";
+return $str;
+        }
+    }
+
+
+$produk1 = new Produk("Narutu","Masahi","Shonen jump","30000");//objek 
+$produk2 = new produk("Atomic Habbit","james clear","indonesia",40000);
+
+
+echo "komik :". $produk1-> penulis, $produk1 -> penerbit;
+echo "<br>";
+echo "Buku ". $produk2->getLabel();
+$infoProduk1 = new CetakInfoTerbaru();
+echo $infoProduk1->cetak($produk);
+ ?>
