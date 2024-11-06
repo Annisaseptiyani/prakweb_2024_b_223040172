07a44f3ae68be4cb69b0accad1c42168db98abdf
Annisaseptiyani <annisa.223040172@mail.unpas.ac.id>
2024-11-06 18:10:23 +0700

    #8 Database Wrapper

--- MVC/App/Core/Constantants.php
+++
@@ -1,3 +0,0 @@
-<?
-
-define('BASEURL','http://localhost/prakweb_2024_b_223040172/MVC/Public');
--- MVC/App/init.php
+++ MVC/App/init.php
@@ -2,5 +2,5 @@
 
 require_once 'core/App.php';
 require_once 'core/Controller.php';
-
-require_once 'core/Constantants.php';
+require_once 'core/Database.php';
+require_once 'confiq/confiq.php';
